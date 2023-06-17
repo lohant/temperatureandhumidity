68b691e94e521409a53847b460cd1c1f4043edd5
Anton Holst <63882500+lohant@users.noreply.github.com>
2023-06-17 15:07:06 +0200

    Create connect.py

---
+++ src/connect.py
@@ -0,0 +1,21 @@
+def do_connect(ssid, password):
+    import network
+    from time import sleep
+
+    wlan = network.WLAN(network.STA_IF)         # Put modem on Station mode
+
+    if not wlan.isconnected():                  # Check if already connected
+        print('connecting to network...')
+        wlan.active(True)                       # Activate network interface
+        # set power mode to get WiFi power-saving off (if needed)
+        wlan.config(pm = 0xa11140)
+        wlan.connect(ssid, password)  # Your WiFi Credential
+        print('Waiting for connection...', end='')
+        # Check if it is connected otherwise wait
+        while not wlan.isconnected() and wlan.status() >= 0:
+            print('.', end='')
+            sleep(1)
+    # Print the IP assigned by router
+    ip = wlan.ifconfig()[0]
+    print('\nConnected on {}'.format(ip))
+    return ip
