23f146c6b77573cd91ec4939b3608aef2d803b8d
Anton Holst <63882500+lohant@users.noreply.github.com>
2023-06-17 15:09:34 +0200

    Create secrets.py

---
+++ src/secrets.py
@@ -0,0 +1,11 @@
+# WiFi Credentials
+secrets_wifi = {
+   'ssid' : 'yourssid',
+   'password' : 'yourpassword',
+   }
+
+# Adafruit IO Credentials
+secrets_aio = {
+   'username' : 'yourusername',
+   'key' : 'yourkey',
+   }
